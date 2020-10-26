e467be080aff5531e2b0581da4b32ea5d44a7c3a
Rodrigo Rocha <47956419+cacotsuki@users.noreply.github.com>
2020-10-25 21:38:46 -0300

    Update copia_para_vienna.sh

--- ferramenta/copia_para_vienna.sh
+++ ferramenta/copia_para_vienna.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-cp /home/ubuntu/ferramenta_final/servidor_apresentacao/ferramenta/resultado_cuda_sankoff.txt /home/ubuntu/ferramenta_final/servidor_apresentacao/ViennaRNA-2.3.3
-cp /home/ubuntu/ferramenta_final/servidor_apresentacao/ferramenta/resultado_cuda_sankoff_upload.txt /home/ubuntu/ferramenta_final/servidor_apresentacao/ViennaRNA-2.3.3
+cp /home/ubuntu/ferramenta_final/servidor_apresentacao/ferramenta/resultado_cuda_sankoff.txt /home/ubuntu/ferramenta_final/servidor_apresentacao/cuda_sankoff/ViennaRNA-2.3.3
+cp /home/ubuntu/ferramenta_final/servidor_apresentacao/ferramenta/resultado_cuda_sankoff_upload.txt /home/ubuntu/ferramenta_final/servidor_apresentacao/cuda_sankoff/ViennaRNA-2.3.3
