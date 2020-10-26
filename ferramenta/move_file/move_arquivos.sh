2741b73908a52039dd132c278102d4b8cd2a2857
Rodrigo Rocha <47956419+cacotsuki@users.noreply.github.com>
2020-10-25 21:26:57 -0300

    Update move_arquivos.sh

--- ferramenta/move_file/move_arquivos.sh
+++ ferramenta/move_file/move_arquivos.sh
@@ -1,1 +1,1 @@
-cp /home/livanski/Music/servidor_apresentacao/servidor_apresentacao/* /home/ubuntu/ferramenta_final/servidor_apresentacao/ViennaRNA-2.3.3
+cp /home/livanski/Music/servidor_apresentacao/servidor_apresentacao/* /home/ubuntu/ferramenta_final/cuda_sankoff/ViennaRNA-2.3.3
