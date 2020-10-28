fead278a2844d99e1f1191bfc9eaf2e4ef09b365
cacotsuki <rodrigo.sp9@hotmail.com>
2020-10-28 20:33:29 -0300

    void

---
+++ ferramenta/move_file/copia_para_vienna.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+cp /home/ubuntu/ferramenta_final/cuda_sankoff/ViennaRNA-2.3.3/rna_convertido.png /home/ubuntu/ferramenta_final/servidor_apresentacao/ferramenta/mysite/static/css/rna_convertido.png
