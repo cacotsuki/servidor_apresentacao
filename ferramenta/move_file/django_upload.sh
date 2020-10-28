b9b4a432ef2b4f2f9675184daa07423b6f6821fa
cacotsuki <rodrigo.sp9@hotmail.com>
2020-10-28 19:16:41 -0300

    void

--- ferramenta/move_file/django_upload.sh
+++ ferramenta/move_file/django_upload.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-convert -density 300 rna.ps -quality 90 -colorspace RGB rna_convertido_upload.png ; cp /home/ubuntu/livanski/cuda_sankoff/ViennaRNA-2.3.3/rna_convertido_upload.png /home/ubuntu/ferramenta_final/servidor_apresentacao/ferramenta/mysite/static/css/rna_convertido_upload.png ; 
+convert -density 300 rna.ps -quality 90 -colorspace RGB rna_convertido_upload.png ; cp /home/ubuntu/ferramenta_final/cuda_sankoff/ViennaRNA-2.3.3/rna_convertido_upload.png /home/ubuntu/ferramenta_final/servidor_apresentacao/ferramenta/mysite/static/css/rna_convertido_upload.png ; 
