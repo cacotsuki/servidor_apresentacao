f132b3132969340b56e4aa7a3c5b4ab7fb462c9f
cacotsuki <rodrigo.sp9@hotmail.com>
2020-10-28 21:55:10 -0300

    void

--- ferramenta/move_file/copia_resultado_cuda.sh
+++ ferramenta/move_file/copia_resultado_cuda.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-cp home/ubuntu/ferramenta_final/servidor_apresentacao/ferramenta/resultado_cuda_sankoff_upload.txt home/ubuntu/ferramenta_final/cuda_sankoff/ViennaRNA-2.3.3/
+cp home/ubuntu/ferramenta_final/servidor_apresentacao/ferramenta/resultado_cuda_sankoff_upload.txt home/ubuntu/ferramenta_final/cuda_sankoff/ViennaRNA-2.3.3
