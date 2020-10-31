be55ecb100b78f9c4e528bf0967e3d153b991ea3
cacotsuki <rodrigo.sp9@hotmail.com>
2020-10-31 11:02:58 -0300

    funcao seq len upload

--- ferramenta/mysite/core/views.py
+++ ferramenta/mysite/core/views.py
@@ -44,8 +44,8 @@ def upload(request):
 
         if seq_len_upload(1) < 41 and seq_len_upload(2) < 41:                           
             return render(request, 'resultado_upload.html',
-                        {'read': converte_to_string_upload(limpar_upload(abre_arquivo_upload())), 'exec': get_exec_time_upload(),
-                        'seq1_len': seq_len_upload(1), 'seq2_len': seq_len_upload(2)})
+                    {'read': converte_to_string_upload(limpar_upload(abre_arquivo_upload())), 'exec': get_exec_time_upload(),
+                    'seq1_len': seq_len_upload(1), 'seq2_len': seq_len_upload(2)})
 
 
         else:
