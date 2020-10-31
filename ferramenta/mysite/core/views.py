17c5053e899ab086553d1171ea3d0a2dcdfafa51
cacotsuki <rodrigo.sp9@hotmail.com>
2020-10-31 10:56:48 -0300

    void

--- ferramenta/mysite/core/views.py
+++ ferramenta/mysite/core/views.py
@@ -45,13 +45,13 @@ def upload(request):
         if seq_len_upload(1) < 41 and seq_len_upload(2) < 41:                           
             return render(request, 'resultado_upload.html',
                         {'read': converte_to_string_upload(limpar_upload(abre_arquivo_upload())), 'exec': get_exec_time_upload(),
-                        'seq1_len': seq_len_upload(1), 'seq2_len': seq_len_upload(2),'nada_faz':remove_fasta_apos_execucao()})
+                        'seq1_len': seq_len_upload(1), 'seq2_len': seq_len_upload(2)})
 
 
         else:
             return render(request, 'mensagem_erro.html')
 
-
+        remove_fasta_apos_execucao()
 
 
 
